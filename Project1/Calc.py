b2d00213f74cec6e15b487bd771a65f29e4f60d1
Precioux <uni.mahdipour@gmail.com>
2022-03-17 12:22:40 +0330

    Final Edition

--- Project1/Calc.py
+++ Project1/Calc.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 class Calculator:
-    def __init__(self, matrix, elements,compounds):
-        self.compounds=compounds
+    def __init__(self, matrix, elements, compounds):
+        self.compounds = compounds
         self.matrix = np.array(matrix)
         self.matrix = self.matrix.astype(float)
         self.elements = elements
@@ -19,12 +19,11 @@ class Calculator:
     def findPivot(self, k):
         i = 0
         flag = False
-        while flag == False:
+        while not flag:
             if self.matrix[i][k] != 0:
                 flag = True
             else:
                 i += 1
-        # print(f"pivot found for{k} is {i}")
         return i
 
     def changeRow(self, i, e):
@@ -32,28 +31,19 @@ class Calculator:
             temp = np.array(self.matrix[e])
             self.matrix[e] = self.matrix[i]
             self.matrix[i] = temp
-        #   print(f"i= {i} => ", self.matrix[i])
-        #  print("temp : ", temp)
-        # print("result of changing rows: ")
         self.check[e] = True
-        # self.printer()
 
     def setOne(self, e):
         a = int(self.matrix[e][e])
         if a != 0:
-            #  print("a is", a)
             tmp = self.matrix[e] / a
             self.matrix[e] = tmp
-        #  print("result of divition: ")
-        # print(self.matrix)
 
     def setZero(self, e):
 
         if e + 1 < self.elements:
             c = e + 1
             while c < self.elements:
-                #  print(f"set to zero for {e} applied on {c}")
-                #  print(self.matrix[c][e])
                 n = int(self.matrix[c][e])
                 self.matrix[c] = self.matrix[c] - self.matrix[e] * n
                 self.setOne(c)
@@ -62,7 +52,6 @@ class Calculator:
     def makeEchlon(self):
         e = 0
         while e < self.elements:
-            #  print(f"for e={e}")
             i = self.findPivot(e)
             self.changeRow(i, e)
             self.setOne(e)
@@ -75,38 +64,39 @@ class Calculator:
     def reduceIt(self):
         t = 0
         while t < self.elements:
-            for i in range(0, self.compounds-1):
+            for i in range(0, self.compounds - 1):
                 n = float(self.matrix[t][i])
                 if n != 0 and t != i:
                     self.matrix[t] = self.matrix[t] - n * self.matrix[i]
             t += 1
 
     def getResults(self):
-            ans=self.checkFree()
-            s=0
-            while s<self.elements:
-                a = float(self.matrix[s][self.compounds - 1])
-                if a<0:
-                    if a==0:
-                        a=float(1)
-                    else:
-                        a=a*(-1)
-                if ans!=-1:
-                    print(f"X{s} = {a} X{ans}")
+        ans = self.checkFree()
+        s = 0
+        while s < self.elements:
+            a = float(self.matrix[s][self.compounds - 1])
+            if a < 0:
+                if a == 0:
+                    a = float(1)
                 else:
-                    print(f"X{s} = {a}")
-                s+=1
-            if ans!=-1:
-                print(f"X{ans} is free")
+                    a = a * (-1)
+            if ans != -1:
+                print(f"X{s} = {a} X{ans}")
+            else:
+                print(f"X{s} = {a}")
+            s += 1
+        if ans != -1:
+            print(f"X{ans} is free")
+
     def checkFree(self):
-        ans=-1
-        for i in range(0,self.compounds):
-            sum=0
+        ans = -1
+        for i in range(0, self.compounds):
+            sum = 0
             for j in range(self.elements):
-                if self.matrix[j][i]!=0 and self.matrix[j][i]!=1:
-                    sum+=1
-            if sum==self.elements:
-                ans=sum
+                if self.matrix[j][i] != 0 and self.matrix[j][i] != 1:
+                    sum += 1
+            if sum == self.elements:
+                ans = sum
         return ans
 
     def ready(self):
--- Project1/main.py
+++ Project1/main.py
@@ -1,91 +1,72 @@
 import numpy as np
 from Calc import Calculator
+
+
 def makeMatrix():
     j = 0
     for element in elements:
-       # print("seraching for "+element)
-        x=[]
+        x = []
         for compound in ingredients:
             flag = False
-            #print(" in ingredients:")
-            #print(compound)
             i = 0
             for c in compound:
-               # print(f"i={i} j={j}")
-                #print(c)
-                if(c==element):
-                    flag=True
-                   # print("YES")
-                    n=len(compound)
-                    if(i+1<n):
-                      #  print("NEXT=> "+compound[i+1])
-                       # print(f"ascii= {ord(compound[i + 1])}")
-                        if(ord(compound[i+1])>=65 and ord(compound[i+1])<=90):
-                            arr=[1]
-                            x=x+arr
+                if c == element:
+                    flag = True
+                    n = len(compound)
+                    if i + 1 < n:
+                        if 65 <= ord(compound[i + 1]) <= 90:
+                            arr = [1]
+                            x = x + arr
                         else:
-                            arrI=[int(compound[i+1])]
-                            x=x+arrI
+                            arrI = [int(compound[i + 1])]
+                            x = x + arrI
                     else:
                         arr = [1]
                         x = x + arr
-                i+=1
-            if(flag==False):
+                i += 1
+            if flag == False:
                 arrZ = [0]
                 x = x + arrZ
 
-
         for compound in products:
             flag = False
-           # print("in products\n")
-           # print(compound)
             i = 0
             for c in compound:
-               # print(f"i={i} j={j}")
-               # print(c)
-                if(c==element):
-                    flag=True
-                  #  print("YES")
-                    n=len(compound)
-                    if(i+1<n):
-                      #  print("NEXT=> "+compound[i+1])
-                      #  print(f"ascii= {ord(compound[i + 1])}")
-                        if(ord(compound[i+1])>=65 and ord(compound[i+1])<=90):
-                            arr=[-1]
-                            x=x+arr
+                if c == element:
+                    flag = True
+                    n = len(compound)
+                    if i + 1 < n:
+                        if 65 <= ord(compound[i + 1]) <= 90:
+                            arr = [-1]
+                            x = x + arr
                         else:
-                            k=int(compound[i+1])
-                            k=k*-1
-                            arrI=[k]
-                            x=x+arrI
+                            k = int(compound[i + 1])
+                            k = k * -1
+                            arrI = [k]
+                            x = x + arrI
                     else:
                         arr = [-1]
                         x = x + arr
 
                 i += 1
-            if(flag==False):
+            if not flag:
                 arrZ = [0]
                 x = x + arrZ
 
         arrf = [0]
         x = x + arrf
-       # print(x)
-        j+=1
+        j += 1
         X.append(x)
-      #  print(X)
 
-elements=input("Enter elements: ")
-equation=input("Enter equation: (example: 𝐶2𝐻6+𝑂2→𝐶𝑂2+𝐻2𝑂)")
-data=equation.split(">")
-ingredients=data[0].split("+")
-products=data[1].split("+")
 
-# print(elements)
-# print(ingredients)
-# print(products)
-shape=(len(elements),len(ingredients)+len(products))
-X=[]
+elements = input("Enter elements: ")
+equation = input("Enter equation: (example: 𝐶2𝐻6+𝑂2>𝐶𝑂2+𝐻2𝑂")
+data = equation.split(">")
+ingredients = data[0].split("+")
+products = data[1].split("+")
+
+shape = (len(elements), len(ingredients) + len(products))
+X = []
 makeMatrix()
-#print(X)
-obj=Calculator(X,len(elements),len(ingredients)+len(products))
+obj = Calculator(X, len(elements), len(ingredients) + len(products))
 obj.ready()
