c0115db37313f87d2a5bf76e488305912c11b52c
Precioux <uni.mahdipour@gmail.com>
2022-05-14 02:37:57 +0430

    part 2 completed!

--- pythonProject3/part2/main.py
+++ pythonProject3/part2/main.py
@@ -3,7 +3,7 @@
 # part II
 import numpy as np
 from PIL import Image
-
+import math
 
 def shear(landa, x, y):
     '''
@@ -16,24 +16,20 @@ def shear(landa, x, y):
 
 
 # load the image
-# address = input("Enter image name ( with file type) :")
-image = np.array(Image.open('part2.jpg'))
-landa = 0.5  # float(input("Enter landa : "))  # Ask the user to enter landa
-
+address = input("Enter image name ( with file type) :")
+image = np.array(Image.open(address))
+landa = float(input("Enter landa : "))  # Ask the user to enter landa
+print("Generating shadow...")
 height = image.shape[0]  # height of the image
 width = image.shape[1]  # width of the image
-print(height)
-print(width)
+
 # Define the height and width of the new image that is to be sheared
 new_height = round(abs(height * (1)) + abs(width * landa)) + 1
 new_width = round(abs(width * 1) + abs(height * 0)) + 1
-print(new_height)
-print(new_width)
-print(image.shape)
+
 # define another image variable of dimensions of new_height and new _column filled with zeros
 output1 = np.zeros((new_height, new_width, image.shape[2]))
 output = np.full_like(output1, 255)
-
 image_copy = output.copy()
 # Find the centre of the image about which we have to rotate the image
 original_centre_height = round(((image.shape[0] + 1) / 2) - 1)  # with respect to the original image
@@ -63,6 +59,33 @@ for i in range(height):
             output[new_y][new_x][0] = 0
             output[new_y][new_x][1] = 0
             output[new_y][new_x][2] = 0
-final = np.concatenate((image,output),axis=1)
+#make final pic backgound
+ximage = image.shape[0]
+yimage = image.shape[1]
+xshadow = output.shape[0]
+yshadow = output.shape[1]
+xmax = max ( ximage , xshadow)
+ymax = max ( yimage , yshadow)
+ymax = ymax + 120
+like = np.zeros((xmax, ymax, image.shape[2]))
+final = np.full_like(like , 255)
+check = np.zeros([xmax,ymax],dtype=int)
+#add original image
+beginX = xmax - ximage
+for i in range(0,ximage):
+    for j in range(0,yimage):
+        final[beginX + i , j,:] = image [ i,j,:]
+        if (240 <= image[i][j][0] <= 255) and (240 <= image[i][j][1] <= 255) and (240 <= image[i][j][2] <= 255):
+            check[beginX+i][j] = 0
+        else:
+            check[beginX+ i][j] = 1
+#add shadow
+for i in range(0,xshadow):
+    for j in range(0,yshadow):
+        if check[i][j+120]==0 :
+            final[i,j+120,:]=output[i,j,:]
+
+# make result image
 result = Image.fromarray((final).astype(np.uint8))  # converting array to image
-result.save(f"sheared_image_{landa}.jpg")  # saving the image
+result.save(f"result.jpg")  # saving the image
+print("Done!")
