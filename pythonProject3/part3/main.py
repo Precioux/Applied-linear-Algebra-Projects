0cc9d358f6471fd62de903d7125943164006c26e
Precioux <uni.mahdipour@gmail.com>
2022-05-14 00:37:17 +0430

    part 3 : trianfular matix is made , determinant is calculater part 3 is done

--- pythonProject3/part3/main.py
+++ pythonProject3/part3/main.py
@@ -14,7 +14,7 @@ print("Enter array C : ")
 for i in range(0, n):
     c[i] = int(input(f"c[{i}]: "))
 # make toeplitz
-toeplitz = np.zeros([n, n], dtype=int)
+toeplitz = np.zeros([n, n], dtype=float)
 # 1) add verctors
 for i in range(0, n):
     toeplitz[i][0] = c[i]
@@ -28,3 +28,21 @@ for i in range(0, n):
 print("Toeplitz matrix : ")
 print(toeplitz)
 # make an upper Triangular
+for i in range(0, n):
+    head = toeplitz[i][i]
+    check = i + 1
+    while check != n :
+        item = toeplitz[check][i]
+        removing = np.zeros(n, dtype=float)
+        removing[0] = float ( (float(toeplitz[i][0]) / head) * item)
+        removing[1] = float ( (float(toeplitz[i][1]) / head) * item)
+        removing[2] = float ( (float(toeplitz[i][2]) / head) * item)
+        toeplitz[check] = toeplitz[check] - removing
+        check = check + 1
+print("Upper triangular matrix")
+print(toeplitz)
+# calculating determinant
+determinant = 1
+for i in range(0,n):
+    determinant = determinant * toeplitz[i][i]
+print(f"Determinant is {determinant}")
